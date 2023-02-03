3176cc383e19154cb9804a8a383cf2f43984e88c
D. V. Wiebe <dvw@phas.ubc.ca>
2023-02-02 16:57:36 -0800

    Docstrings

--- chimedb/core/alpenhorn.py
+++ chimedb/core/alpenhorn.py
@@ -1,6 +1,11 @@
 """Alpenhorn-2 integration.
 
-Provides an extension module for alpenhorn-2.
+Provides a database extension module for alpenhorn-2 which
+connects chimedb and alpenhorn, allowing alpenhorn to use this
+package for database access.
+
+See `alpenhorn.extensions` and `alpenhorn.db` for more details
+on what an alpenhorn database extension is.
 """
 
 import peewee as pw
@@ -10,7 +15,26 @@ from .exceptions import ConnectionError, NoRouteToDatabase
 
 
 def connect(config):
-    """connectdb.connect() boilperplate"""
+    """Connect to the CHIMEDB for alpenhorn use.
+
+    This is a wrapper around `connectdb.connect` which will be called by
+    `alpenhorn` to establish a database connection.
+
+    The `peewee.Database` from the read-write connector is returned
+    to alpenhorn.
+
+    Parameters
+    ----------
+    config : dict
+        Ignored.  This is the parsed contents of the "database" section of the
+        alpenhorn config file.  This function ignores this configuration
+        data: set up CHIMEDB in the usual way to use it with alpenhorn.
+
+    Raises
+    ------
+    peewee.OperationalError
+        On database connection failure
+    """
     try:
         connectdb.connect()
     except (NoRouteToDatabase, ConnectionError) as e:
@@ -26,7 +50,11 @@ def connect(config):
 
 
 def register_extension():
-    """Register an alpenhorn database extension."""
+    """Register an alpenhorn database extension.
+
+    Returns a "capability dict" providing the database capabilities of this
+    database module to alpenhorn.
+    """
     return {
         "database": {"connect": connect, "close": connectdb.close, "reentrant": True}
     }
